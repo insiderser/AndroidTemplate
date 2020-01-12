e920ccf9c9b439fe00d350e11e1308cddc4a3ff2
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-01-12 20:30:37 +0200

    [fix] Add initialization vector for encryption

--- release/secrets-setup.sh
+++ release/secrets-setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ -n $RELEASE_KEYSTORE_ENCRYPT_KEY ]]; then
+if [[ -n $RELEASE_KEYSTORE_ENCRYPT_KEY && -n $RELEASE_KEYSTORE_IV ]]; then
     openssl aes-256-cbc -md sha256 -d -in release/release.jks.aes \
-        -out release/release.jks -K "$RELEASE_KEYSTORE_ENCRYPT_KEY" -v -iter 1000000
+        -out release/release.jks -K "$RELEASE_KEYSTORE_ENCRYPT_KEY" \
+        -iv "$RELEASE_KEYSTORE_IV" -iter 1000000
 else
-    echo The RELEASE_KEYSTORE_ENCRYPT_KEY is empty
+    echo Keys are empty
+    exit 1
 fi
