f7c4cdc967261464719da0165b4809092bc61988
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-01-12 21:43:54 +0200

    Switch to password decryption

--- release/mapping-archivator.sh
+++ release/mapping-archivator.sh
@@ -6,7 +6,7 @@ zip -r -j app/build/outputs/mapping/release.zip app/build/outputs/mapping/releas
 #Encrypt zipped mapping
 openssl enc -e -aes-256-cbc -in app/build/outputs/mapping/release.zip \
     -out app/build/outputs/mapping/release.zip.enc -v -iter 1000000 \
-    -k "$MAPPING_PASSPHRASE"
+    -k "$MAPPING_PASSPHRASE" -md sha256
 
 # Remove mapping to make sure that we don't leak it anywhere
 rm -rf app/build/outputs/mapping/release/ app/build/outputs/mapping/release.zip
--- release/secrets-setup.sh
+++ release/secrets-setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ -n $RELEASE_KEYSTORE_KEY && -n $RELEASE_KEYSTORE_IV ]]; then
+if [[ -n $RELEASE_KEYSTORE_KEY ]]; then
     openssl aes-256-cbc -md sha256 -d -in release/release.jks.enc \
-        -out release/release.jks -K "$RELEASE_KEYSTORE_KEY" \
-        -iv "$RELEASE_KEYSTORE_IV" -iter 1000000
+        -out release/release.jks -k "$RELEASE_KEYSTORE_KEY" -iter 1000000
 else
     echo Keys are empty
     exit 1
