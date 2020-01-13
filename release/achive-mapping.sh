04d260ba64e4e28ed2ba067043749ed1ad56ce34
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-01-13 10:04:16 +0200

    [ci] Switch to key & iv

--- release/achive-mapping.sh
+++ release/achive-mapping.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-MAPPING_PASSPHRASE="$*"
+MAPPING_KEY="$1"
+MAPPING_IV="$2"
 
-if [[ -n $MAPPING_PASSPHRASE ]]; then
+if [[ -n $MAPPING_KEY ]]; then
     # Zip mapping
     zip -r -j app/build/outputs/mapping/release.zip app/build/outputs/mapping/release/
 
     # Encrypt zipped mapping
     openssl enc -e -aes-256-cbc -in app/build/outputs/mapping/release.zip \
         -out app/build/outputs/mapping/release.zip.enc -v -iter 1000000 \
-        -k "$MAPPING_PASSPHRASE" -md sha256
+        -K "$MAPPING_KEY" -iv "$MAPPING_IV" -md sha256
 fi
 
 # Remove mapping to make sure that we don't leak it anywhere
--- release/secrets-setup.sh
+++ release/secrets-setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-RELEASE_KEYSTORE_KEY="$*"
+RELEASE_KEYSTORE_KEY="$1"
+RELEASE_KEYSTORE_IV="$2"
 
-if [[ -n $RELEASE_KEYSTORE_KEY ]]; then
+if [[ -n $RELEASE_KEYSTORE_KEY && -n $RELEASE_KEYSTORE_IV ]]; then
     openssl enc -aes-256-cbc -md sha256 -d -in release/release.jks.enc \
-        -out release/release.jks -k "$RELEASE_KEYSTORE_KEY" -iter 1000000
+        -out release/release.jks -K "$RELEASE_KEYSTORE_KEY" -iter 1000000 \
+        -iv "$RELEASE_KEYSTORE_IV"
 else
     echo RELEASE_KEYSTORE_KEY is empty
     exit 1
