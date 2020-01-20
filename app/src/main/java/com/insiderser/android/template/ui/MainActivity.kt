3127fd78c3d1c4b6d16fec4a56fa05ee7e370927
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-01-20 18:25:13 +0200

    Add Toolbar to MainActivity

--- app/src/main/java/com/insiderser/android/template/ui/MainActivity.kt
+++ app/src/main/java/com/insiderser/android/template/ui/MainActivity.kt
@@ -50,6 +50,8 @@ class MainActivity : DaggerAppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         binding.root.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
