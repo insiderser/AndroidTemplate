a28d33712eb0cc0800cfd961cfd51b85194813bb
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-03-19 15:08:24 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/com/insiderser/android/template/buildSrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/insiderser/android/template/buildSrc/Dependencies.kt
@@ -129,7 +129,7 @@ object Libs {
         }
 
         object Room {
-            private const val version = "2.2.4"
+            private const val version = "2.2.5"
             const val runtime = "androidx.room:room-runtime:$version"
             const val compiler = "androidx.room:room-compiler:$version"
             const val ktx = "androidx.room:room-ktx:$version"
