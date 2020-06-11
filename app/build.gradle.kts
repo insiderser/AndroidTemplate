6e21e6c5ae9931a730a78c24911793e75541b0e3
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-11 17:05:22 +0300

    !fixup Fix Android tests compilation error
    WFT Google?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,12 +67,6 @@ android {
         isCheckDependencies = true
         lintConfig = rootProject.file("lint.xml")
     }
-
-    packagingOptions {
-        exclude("META-INF/AL2.0")
-        exclude("META-INF/LGPL2.1")
-        exclude("META-INF/*.kotlin_module")
-    }
 }
 
 dependencies {
--- buildSrc/src/main/kotlin/com/insiderser/android/template/buildSrc/Configuration.kt
+++ buildSrc/src/main/kotlin/com/insiderser/android/template/buildSrc/Configuration.kt
@@ -79,5 +79,12 @@ fun Project.configureAndroidModule() {
             animationsDisabled = true
             unitTests.isIncludeAndroidResources = true
         }
+
+        // FIXME: Android Studio bug?
+        packagingOptions {
+            exclude("META-INF/AL2.0")
+            exclude("META-INF/LGPL2.1")
+            exclude("META-INF/*.kotlin_module")
+        }
     }
 }
